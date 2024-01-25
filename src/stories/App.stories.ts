72e4b6442251ba51ddc93069b1179bdfcc722e6c
Nick Yanush <m.yanush@apiko.com>
2024-01-25 14:32:02 +0200

    Added storybook

---
+++ src/stories/App.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { App } from '../App';
+
+const meta = {
+	title: 'Example/App',
+	component: App,
+	tags: ['autodocs']
+} satisfies Meta<typeof App>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Page: Story = {
+	args: {
+		label: 'Page view'
+	}
+};
